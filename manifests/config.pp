1a6e2c7c88bf43756fef5654d33ea738e05377cd
Joshua Rich <joshua.rich@gmail.com>
2018-02-18 00:09:35 +1100

    Fix merging of Beats settings.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,13 +2,13 @@
 # This class handles the configuration files for beats. Avoid modifying private classes.
 class beats::config {
   $beats::beats_manage.each |String $beat| {
-    if lookup("beats::${beat}::settings", Data, 'hash', {}) != {} {
+    if lookup("beats::${beat}::settings", Hash, 'deep', {}) != {} {
       file { "${beats::config_root}/${beat}/${beat}.yml":
         ensure  => file,
         owner   => 0,
         group   => 0,
         mode    => '0600',
-        content => inline_epp('<%= lookup("beats::${beat}::settings", {}).to_yaml %>'),
+        content => inline_epp('<%= lookup("beats::${beat}::settings", Hash, \'deep\').to_yaml %>'),
       }
     }
     elsif lookup("beats::${beat}::settings", String, unique, '') != '' {
